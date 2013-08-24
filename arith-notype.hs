b5fdd6f2f78ad418fc276485f55d9e539ffafb7b
na2hiro <a@81.la>
2013-08-25 03:06:24 +0900

    value消す．評価不能をNothingとした．

--- arith-notype.hs
+++ arith-notype.hs
@@ -1,42 +1,35 @@
-import Prelude hiding (True, False)
-data Term = True
-          | False
+data Term = Tru
+          | Fals
           | If Term Term Term
           | Zero
           | Succ Term
           | Pred Term
           | IsZero Term
+          | Var Integer
+          | Abs Term
+          | App Term Term
           deriving Show
 
-data Value = VTrue
-           | VFalse
-           | VNum VNum
-           deriving Show
+eval1 :: Term->Maybe Term
+eval1 (If Tru t _) = Just t
+eval1 (If Fals _ t) = Just t
+{-
+isJust (Just _) = True
+isJust Nothing = False
+eval1 (If t b c) | isJust new = new >>= (\newt->Just$ If newt b c)
+  where new = eval1 t
+-}
+eval1 (If t b c) = eval1 t >>= (\newt->Just$ If newt b c)
+eval1 (Succ t) = eval1 t >>= Just. Succ
+eval1 (Pred Zero) = Just Zero
+eval1 (Pred (Succ n)) = Just n
+eval1 (Pred t) = eval1 t >>= Just. Pred
+eval1 (IsZero Zero) = Just Tru
+eval1 (IsZero (Succ n)) = Just Fals
+eval1 (IsZero t) = eval1 t >>= Just. IsZero
+eval1 _ = Nothing
 
-data VNum = VZero
-          | VSucc VNum
-          deriving Show
-
-eval1 :: Term->Term
-eval1 (If True t _) = t
-eval1 (If False _ t) = t
-eval1 (If a b c) = If newa b c
-  where newa = eval1 a
-eval1 (Succ t) = Succ newt
-  where newt = eval1 t
-eval1 (Pred Zero) = Zero
-eval1 (Pred (Succ n)) = n
-eval1 (Pred t) = Pred newt
-  where newt = eval1 t
-eval1 (IsZero Zero) = True
-eval1 (IsZero (Succ n)) = False
-eval1 (IsZero t) = IsZero newt
-  where newt = eval1 t
-
-eval :: Term->Value
-eval True = VTrue
-eval False = VFalse
-eval Zero = VNum VZero
-eval (Succ t) = VNum$ VSucc newt
-  where (VNum newt) = eval t
-eval t = eval (eval1 t)
+eval :: Term->Term
+eval t = case (eval1 t) of
+           Just t2->eval t2
+           Nothing->t
